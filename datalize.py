41cad53a069f183a3ba87f0259f4e4b20e0fe631
50m-regent <len.hirata@gmail.com>
2023-08-10 01:03:37 +0900

    tenhou data init made

--- datalize.py
+++ datalize.py
@@ -1,6 +1,23 @@
 import gzip
 from defusedxml import ElementTree
 
+class State:
+    @classmethod
+    def id2onehot(cls, tile):
+        result = [0] * 34
+        result[tile // 4] = 1
+        
+        return result
+    
+    def __init__(self):
+        self.bonus_tiles = [0] * 5
+    
+    def __str__(self):
+        return \
+            '-----------------------\n' + \
+            f'Round: {self.round}, Stack: {self.stack}, Bet: {self.bet}, Bonus: {self.bonus_tiles}, Oya: {self.oya}, Score: {self.scores}\n' + \
+            f'Hand: {self.hands}'
+
 class MjlogReader:
     IGNORE_TAGS = [
         'mjloggm',
@@ -16,29 +33,43 @@ class MjlogReader:
             for line in f:
                 self.tree = ElementTree.fromstring(line)
                 
+        self.state = State()
+                
     def init(self, attrib):
-        print('init', attrib)
+        self.state.round, self.state.stack, self.state.bet, _, _, bonus_tile = eval(attrib['seed'])
+        self.state.bonus_tiles[0] = bonus_tile // 4
+        
+        self.state.scores = eval(attrib['ten'])
+        
+        self.state.oya = int(attrib['oya'])
+        
+        self.state.hands = [
+            [tile // 4 for tile in eval(attrib['hai0'])],
+            [tile // 4 for tile in eval(attrib['hai1'])],
+            [tile // 4 for tile in eval(attrib['hai2'])],
+            [tile // 4 for tile in eval(attrib['hai3'])]
+        ]
     
     def reach(self, attrib):
         print('reach', attrib)
         
     def n(self, attrib):
-        print('n', attrib)
+        pass
         
     def dora(self, attrib):
-        print('dora', attrib)
+        pass
         
     def agari(self, attrib):
-        print('agari', attrib)
+        pass
         
     def ryuukyoku(self, attrib):
-        print('ryuukyoku', attrib)
+        pass
         
     def draw(self, tag):
-        print('draw', tag)
+        pass
         
     def discard(self, tag):
-        print('discard', tag)
+        pass
         
     def analyze(self):
         for i in self.tree.iter():
@@ -61,6 +92,8 @@ class MjlogReader:
                 self.draw(i.tag)
             elif i.tag[0] in 'DEFG':
                 self.discard(i.tag)
+                
+            # print(self.state)
     
 
 if __name__ == '__main__':
